3269461bb487234d5a82465018a51a985b559832
lykmapipo <lallyelias87@gmail.com>
2018-09-14 21:06:49 +0300

    Upgrade
    - Force latest dependencies
    - Add indexes to fields

--- test/bootstrap.spec.js
+++ test/bootstrap.spec.js
@@ -4,12 +4,13 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+
 before(function(done) {
-  mongoose.connect('mongodb://localhost/irina', done);
+    mongoose.connect('mongodb://localhost/irina', { useNewUrlParser: true }, done);
 });
 
 
 //clean database
 after(function(done) {
-  mongoose.connection.dropDatabase(done);
+    mongoose.connection.dropDatabase(done);
 });
